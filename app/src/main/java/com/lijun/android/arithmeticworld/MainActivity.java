906ea653f18603b2014122766c215649996aceef
wogua <514285768@qq.com>
2018-08-20 15:15:12 +0800

    init base code with bottombar

---
+++ app/src/main/java/com/lijun/android/arithmeticworld/MainActivity.java
@@ -0,0 +1,34 @@
+package com.lijun.android.arithmeticworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lijun.android.arithmeticworld.fragment.Fragment1;
+import com.lijun.android.arithmeticworld.fragment.Fragment2;
+import com.lijun.android.arithmeticworld.fragment.Fragment3;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomBar bottomBar = findViewById(R.id.bottom_bar);
+        bottomBar.setContainer(R.id.fl_container)
+                .setTitleBeforeAndAfterColor("#999999", "#ff5d5e")
+                .addItem(Fragment1.class,
+                        "首页",
+                        R.drawable.item1_before,
+                        R.drawable.item1_after)
+                .addItem(Fragment2.class,
+                        "订单",
+                        R.drawable.item2_before,
+                        R.drawable.item2_after)
+                .addItem(Fragment3.class,
+                        "我的",
+                        R.drawable.item3_before,
+                        R.drawable.item3_after)
+                .build();
+    }
+}
---
+++ app/src/main/java/com/lijun/android/arithmeticworld/fragment/Fragment1.java
@@ -0,0 +1,17 @@
+package com.lijun.android.arithmeticworld.fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lijun.android.arithmeticworld.R;
+
+public class Fragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return LayoutInflater.from(getActivity()).inflate(R.layout.fragment1, container, false);
+    }
+}
